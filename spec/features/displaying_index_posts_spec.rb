a53b76232a76bc3403e9e33528791068e95b928b
Andrew Neidley <neidz44@gmail.com>
2017-10-16 20:23:28 -0500

    index loops through posts

--- spec/features/displaying_index_posts_spec.rb
+++ spec/features/displaying_index_posts_spec.rb
@@ -1,7 +1,13 @@
-feature 'Displaying posts' do
-  scenario 'view all posts image and comments' do
+require 'rails_helper.rb'
+
+feature 'Index displays a list of posts' do
+  scenario 'the index displays correct created post information' do
+    post_one = create(:post, caption: "This is post one")
+    post_two = create(:post, caption: "This is the second post")
+
     visit '/'
-    expect(page).to have_content('#coffeetime')
+    expect(page).to have_content('This is post one')
+    expect(page).to have_content('This is the second post')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
 end
